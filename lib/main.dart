cc02da0678359037436ebe403da82ffe13dbf7d9
cnoah <noah.charrinbourrat@ynov.com>
2025-11-23 20:19:44 +0100

    fix: const removed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      home: const LoginPage(),
+      home: LoginPage(),
     );
   }
 }
