f2eb414bd196fabdc26f08f821f16d1d3a2b0130
cnoah <noah.charrinbourrat@ynov.com>
2025-11-23 20:20:06 +0100

    feat: add MenuBottomNavBar widget with gradient styling

---
+++ lib/components/menu_bottom_nav_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'bottom_nav_bar_button.dart';
+import 'color_builders.dart';
+
+class MenuBottomNavBar extends StatelessWidget {
+  final String buttonText;
+
+  const MenuBottomNavBar({super.key, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(
+        gradient: buildMenuAppBarAndBottomNavBarGradient(true),
+        border: Border(top: BorderSide(color: Colors.black, width: 4)),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: SafeArea(child: BottomNavBarButton(buttonText: buttonText)),
+    );
+  }
+}
