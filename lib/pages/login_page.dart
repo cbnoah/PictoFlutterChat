4b33584949a62676a847fb6df0212a3160e5144e
cnoah <noah.charrinbourrat@ynov.com>
2025-11-23 20:19:31 +0100

    feat: add MenuBottomNavBar component and integrate it into the LoginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:picto_flutter_chat/components/confirmation_button.dart';
 import 'package:picto_flutter_chat/components/menu_app_bar.dart';
+import 'package:picto_flutter_chat/components/menu_bottom_nav_bar.dart';
 import '../components/account_text_bar.dart';
 import '../components/horizontal_lines_background_painter.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode (SystemUiMode.manual, overlays: []);
     return Scaffold(
       appBar: const MenuAppBar(title: 'Welcome back'),
       backgroundColor: Theme.of(context).colorScheme.surface,
@@ -30,12 +38,12 @@ class _LoginPageState extends State<LoginPage> {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: const Center(
+              child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   spacing: 50.0,
                   children: [
-                    Padding(
+                    const Padding(
                       padding: EdgeInsets.only(bottom: 20),
                       child: Text(
                         'Login to your account',
@@ -53,14 +61,27 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       spacing: 30.0,
                       children: [
-                        AccountTextBar(labelText: "Username or Email"),
-                        AccountTextBar(labelText: "Password"),
+                        AccountTextBar(
+                          labelText: "Username or Email",
+                          obscureText: false,
+                          controller: widget._usernameController,
+                        ),
+                        AccountTextBar(
+                          labelText: "Password",
+                          obscureText: true,
+                          controller: widget._passwordController,
+                        ),
                         ConfirmationButton(buttonText: "Log in"),
                       ],
                     ),
+
                   ],
                 ),
               ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: MenuBottomNavBar(buttonText: "Sign Up"),
             ),
           ],
         ),
