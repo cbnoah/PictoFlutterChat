a4e21d58ee1718e45b3e8f450bda12a14a1bfed6
cnoah <noah.charrinbourrat@ynov.com>
2025-12-08 22:35:40 +0100

    feat: update assets section in pubspec.yaml to include logo directory

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:picto_flutter_chat/components/confirmation_button.dart';
+import 'package:picto_flutter_chat/components/login_with_buttons.dart';
 import 'package:picto_flutter_chat/components/menu_app_bar.dart';
 import 'package:picto_flutter_chat/components/menu_bottom_nav_bar.dart';
 import '../components/account_text_bar.dart';
@@ -21,31 +22,34 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode (SystemUiMode.manual, overlays: []);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return Scaffold(
       appBar: const MenuAppBar(title: 'Welcome back'),
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            CustomPaint(
-              painter: HorizontalLinesBackgroundPainter(
-                spacing: 20,
-                lineWidth: 1,
-                lineColor: Color(0xFFcac4d0),
-              ),
-              size: Size.infinite,
+      body: Stack(
+        children: [
+          CustomPaint(
+            painter: HorizontalLinesBackgroundPainter(
+              spacing: 20,
+              lineWidth: 1,
+              lineColor: Color(0xFFcac4d0),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
+            size: Size.infinite,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom == 0 ? 80.0 : 0,
+              ),
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 50.0,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 20),
-                      child: Text(
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 50.0,
+                    children: [
+                      Text(
                         'Login to your account',
                         textAlign: TextAlign.center,
                         style: TextStyle(
@@ -56,35 +60,67 @@ class _LoginPageState extends State<LoginPage> {
                           color: Colors.black,
                         ),
                       ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      spacing: 30.0,
-                      children: [
-                        AccountTextBar(
-                          labelText: "Username or Email",
-                          obscureText: false,
-                          controller: widget._usernameController,
-                        ),
-                        AccountTextBar(
-                          labelText: "Password",
-                          obscureText: true,
-                          controller: widget._passwordController,
-                        ),
-                        ConfirmationButton(buttonText: "Log in"),
-                      ],
-                    ),
-
-                  ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 20.0,
+                        children: [
+                          AccountTextBar(
+                            labelText: "Username or Email",
+                            obscureText: false,
+                            controller: widget._usernameController,
+                          ),
+                          AccountTextBar(
+                            labelText: "Password",
+                            obscureText: true,
+                            controller: widget._passwordController,
+                          ),
+                          ConfirmationButton(buttonText: "Log in"),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Divider(),
+                          Text(
+                            "Login with :",
+                            style: TextStyle(
+                              fontFamily: "Nintendo_DS_Bios",
+                              fontSize: 35,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              LoginWithButtons(
+                                buttonText: "Google",
+                                redirectPage: Placeholder(),
+                                imagePath: "assets/logo/google_pixel_logo.png",
+                                backgroundColor: Color(0xFFd9d9d9),
+                              ),
+                              LoginWithButtons(
+                                buttonText: "Discord",
+                                redirectPage: Placeholder(),
+                                imagePath: "assets/logo/discord_pixel_logo.png",
+                                backgroundColor: Color(0xFF5662F6),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
+          ),
+          if (MediaQuery.of(context).viewInsets.bottom == 0)
             Align(
               alignment: Alignment.bottomCenter,
               child: MenuBottomNavBar(buttonText: "Sign Up"),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
