600b9ecd90ce02f88c0b599994c73fbfd2596d71
cnoah <noah.charrinbourrat@ynov.com>
2025-12-22 22:09:27 +0100

    feat: implement reset password screen with email input and background design

--- lib/pages/reset_password.dart
+++ lib/pages/reset_password.dart
@@ -2,6 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:picto_flutter_chat/utils/auth.dart';
 
+import '../components/account_text_bar.dart';
+import '../components/color_builders.dart';
+import '../components/confirmation_button.dart';
+import '../components/horizontal_lines_background_painter.dart';
+import '../components/menu_app_bar.dart';
+
 class ResetPassword extends StatefulWidget {
   const ResetPassword({super.key});
 
@@ -29,6 +35,67 @@ class _ResetPasswordState extends State<ResetPassword> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: const MenuAppBar(title: 'Forgot your password ?'),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Stack(
+        children: [
+          CustomPaint(
+            painter: HorizontalLinesBackgroundPainter(
+              spacing: 20,
+              lineWidth: 1,
+              lineColor: const Color(0xFFcac4d0),
+            ),
+            size: Size.infinite,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom == 0
+                    ? 80.0
+                    : 0,
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20.0,
+                  children: [
+                    Text(
+                      'Enter your email to reset your password',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "Nintendo_DS_Bios",
+                        fontWeight: FontWeight.w700,
+                        fontSize: 55,
+                        height: 1,
+                        color: Colors.black,
+                      ),
+                    ),
+                    AccountTextBar(
+                      controller: _emailController,
+                      labelText: 'Email',
+                      obscureText: false,
+                      isPasswordField: false,
+                    ),
+                    ConfirmationButton(
+                      buttonText: "Send reset link",
+                      action: () => resetPassword(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          gradient: buildMenuAppBarAndBottomNavBarGradient(true),
+          border: Border(top: BorderSide(color: Colors.black, width: 4)),
+        ),
+      ),
+    );
   }
 }
