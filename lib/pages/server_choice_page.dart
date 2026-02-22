a5e468edac8afc1fcc151ee10d5632955514e73b
cnoah <noah.charrinbourrat@ynov.com>
2026-02-22 23:35:45 +0100

    feat: enhance ServerChoicePage with chat room buttons and logout functionality

--- lib/pages/server_choice_page.dart
+++ lib/pages/server_choice_page.dart
@@ -1,6 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:picto_flutter_chat/utils/auth.dart';
+import 'package:picto_flutter_chat/components/chat_room_button.dart';
+import 'package:picto_flutter_chat/components/menu_bottom_nav_bar.dart';
+import 'package:picto_flutter_chat/utils/auth_service.dart';
+
+import '../components/horizontal_lines_background_painter.dart';
+import '../components/menu_app_bar.dart';
 
 class ServerChoicePage extends StatefulWidget {
   const ServerChoicePage({super.key});
@@ -10,19 +14,67 @@ class ServerChoicePage extends StatefulWidget {
 }
 
 class _ServerChoicePageState extends State<ServerChoicePage> {
+  final authService = AuthService();
+
   void logout() async {
-    try {
-      await authService.value.signOut();
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-    }
+    await authService.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FilledButton(onPressed: () => logout(), child: Text("Log out")),
+      appBar: const MenuAppBar(title: 'Choose a Chat Room to join'),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Stack(
+        children: [
+          CustomPaint(
+            painter: HorizontalLinesBackgroundPainter(
+              spacing: 20,
+              lineWidth: 1,
+              lineColor: Color(0xFFcac4d0),
+            ),
+            size: Size.infinite,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 25,
+                children: [
+                  ChatRoomButton(
+                    chatRoomName: "A",
+                    messageCount: refactorMessageCount(12),
+                  ),
+                  ChatRoomButton(
+                    chatRoomName: "B",
+                    messageCount: refactorMessageCount(1051),
+                  ),
+                  ChatRoomButton(
+                    chatRoomName: "C",
+                    messageCount: refactorMessageCount(153),
+                  ),
+                  ChatRoomButton(
+                    chatRoomName: "D",
+                    messageCount: refactorMessageCount(12),
+                  ),
+                  //ElevatedButton(onPressed: logout, child: Text('Logout')),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: MenuBottomNavBar(
+        buttonText: "Settings",
+        redirectPage: Scaffold(
+          appBar: AppBar(title: Text("Settings"),),
+          body: Center(child: FilledButton(onPressed: () {
+            logout();
+            Navigator.pop(context);
+          }, child: Text("Log out")),),
+        ),
+        icon: Icon(Icons.settings),
       ),
     );
   }
