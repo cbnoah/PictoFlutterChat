d873db7b6a0e807fa2d143bd5e0a9a2ffea4f3b6
cnoah <noah.charrinbourrat@ynov.com>
2026-02-22 23:35:25 +0100

    feat: replace Firebase initialization with Supabase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:picto_flutter_chat/utils/auth_layout.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:picto_flutter_chat/theme/light_theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: 'https://fukfxrdnjnidqpgqtbtp.supabase.co',
+    anonKey: 'sb_publishable_dNlnFts7H19CqPOocML9Gw_8TUF1rwC',
+  );
   runApp(const MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
